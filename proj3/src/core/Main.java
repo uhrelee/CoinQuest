a994cdf24c38917f4e07a90382de7a72e763f4f2
Arely <uhrelee@berkeley.edu>
2024-07-24 16:39:11 -0700

    finished hallways and walls

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -9,10 +9,10 @@ import java.util.*;
 //
 public class Main {
 
-    private static final int WIDTH = 60;
-    private static final int HEIGHT = 40;
-    private static final long SEED = 987654;
-    private static final Random RANDOM = new Random(SEED);
+    private static final int WIDTH = 65;
+    private static final int HEIGHT = 55;
+    private static final long SEED = 666;
+    private static final Random rand = new Random(SEED);
     private static final List<Rooms> rooms = new ArrayList<>();
 
 
@@ -28,31 +28,39 @@ public class Main {
         }
 
         fillWithSeveralRooms(world);
+        connectRooms(world);
+        buildWalls(world);
+
+        while (emptySpaceProportion(world) > 0.5) {
+            fillWithSeveralRooms(world);
+            connectRooms(world);
+            buildWalls(world);
+        }
+        System.out.println("Proportion: " + emptySpaceProportion(world));
         ter.renderFrame(world);
     }
 
-    public static double emptySpaceProportion(TETile[][] world){
+    public static double emptySpaceProportion(TETile[][] world) {
         int count = 0;
-        for (int x = 0; x < WIDTH; x ++){
-            for (int y = 0; y < HEIGHT; y++){
-                if (world[x][y] == Tileset.NOTHING){
-                    count ++;
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (world[x][y] == Tileset.NOTHING) {
+                    count++;
                 }
             }
         }
         int area = HEIGHT * WIDTH;
-        return count / area;
+        return (double) count / area;
     }
 
-    public static int generateRoomNumber(){
+    public static int generateRoomNumber() {
         List<Integer> list = new ArrayList<>();
-        for (int i = 10; i <= 20; i++) {
+        for (int i = 5; i <= 10; i++) {
             list.add(i);
         }
-        Random rand = new Random();
-        int randomelement = list.get(rand.nextInt(list.size()));
-        System.out.println("Number of rooms" + randomelement);
-        return randomelement;
+        int randomElement = list.get(rand.nextInt(list.size()));
+        System.out.println("Number of rooms" + randomElement);
+        return randomElement;
     }
 
 
@@ -77,25 +85,23 @@ public class Main {
 
     public static void fillWithSeveralRooms(TETile[][] tiles) {
         int numberOfRooms = generateRoomNumber();
-        for (int i = 0; i < numberOfRooms; i++){
+        for (int i = 0; i < numberOfRooms; i++) {
             fillWithRandomTiles(tiles);
         }
     }
 
     private static int randomTileHeight() {
-        List<Integer> list = Arrays.asList(5, 6, 7,  8, 9, 10, 11);
-        Random rand = new Random();
-        int randomelement = list.get(rand.nextInt(list.size()));
-        System.out.println(randomelement);
-        return randomelement;
-        }
+        List<Integer> list = Arrays.asList(4, 5, 6, 7,  8, 9, 10);
+        int randomElement = list.get(rand.nextInt(list.size()));
+        System.out.println(randomElement);
+        return randomElement;
+    }
 
     private static int randomTileWidth() {
-        List<Integer> list = Arrays.asList(5, 6, 7,  8, 9, 10, 11);
-        Random rand = new Random();
-        int randomelement = list.get(rand.nextInt(list.size()));
-        System.out.println(randomelement);
-        return randomelement;
+        List<Integer> list = Arrays.asList(4, 5, 6, 7,  8, 9, 10);
+        int randomElement = list.get(rand.nextInt(list.size()));
+        System.out.println(randomElement);
+        return randomElement;
     }
 
     private static int chooseRoomCenterX() {
@@ -105,27 +111,84 @@ public class Main {
         for (int i = width/2 + 3; i <= end - width/2 - 3; i++) {
             list.add(i);
         }
-        Random rand = new Random();
-        int randomelement = list.get(rand.nextInt(list.size()));
-        System.out.println("Random X cootdinate" + randomelement);
-        return randomelement;
+        int randomElement = list.get(rand.nextInt(list.size()));
+        System.out.println("Random X cootdinate" + randomElement);
+        return randomElement;
     }
 
-    private static int chooseRoomCenterY(){
+    private static int chooseRoomCenterY() {
         List<Integer> list = new ArrayList<>();
         int end = HEIGHT;
         int height = randomTileHeight();
         for (int i = height/2 + 3; i <= end - height/2 - 3; i++) {
             list.add(i);
         }
-        Random rand = new Random();
-        int randomelement = list.get(rand.nextInt(list.size()));
-        System.out.println("Random Y cootdinate" + randomelement);
-        return randomelement;
+        int randomElement = list.get(rand.nextInt(list.size()));
+        System.out.println("Random Y cootdinate" + randomElement);
+        return randomElement;
+    }
+    private static void connectRooms(TETile[][] tiles) {
+        for (int i = 0; i < rooms.size() - 1; i++) {
+            Rooms room1 = rooms.get(i);
+            Rooms room2 = rooms.get(i + 1);
+            drawHallway(tiles, room1.getCenterX(), room1.getCenterY(),room2.getCenterX(), room2.getCenterY());
+        }
     }
 
-
-
+    private static void drawHallway(TETile[][] tiles, int x1, int y1, int x2, int y2) {
+        if (x1 != x2) {
+            if (x1 < x2) {
+                for (int x = x1; x <= x2; x++) {
+                    tiles[x][y1] = Tileset.FLOWER;
+                }
+            } else {
+                for (int x = x2; x <= x1; x++) {
+                    tiles[x][y1] = Tileset.FLOWER;
+                }
+            }
+        }
+        if (y1 != y2) {
+            if (y1 < y2) {
+                for (int y = y1; y <= y2; y++) {
+                    tiles[x2][y] = Tileset.FLOWER;
+                }
+            } else {
+                for (int y = y2; y <= y1; y++) {
+                    tiles[x2][y] = Tileset.FLOWER;
+                }
+            }
+        }
     }
-
-
+    private static void buildWalls(TETile[][] world) {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (world[x][y] == Tileset.FLOWER) {
+                    if (x > 0 && world[x - 1][y] == Tileset.NOTHING) {
+                        world[x - 1][y] = Tileset.WALL;
+                    }
+                    if (x < WIDTH - 1 && world[x + 1][y] == Tileset.NOTHING) {
+                        world[x + 1][y] = Tileset.WALL;
+                    }
+                    if (y > 0 && world[x][y - 1] == Tileset.NOTHING) {
+                        world[x][y - 1] = Tileset.WALL;
+                    }
+                    if (y < HEIGHT - 1 && world[x][y + 1] == Tileset.NOTHING) {
+                        world[x][y + 1] = Tileset.WALL;
+                    }
+                    if (x > 0 && y > 0 && world[x - 1][y - 1] == Tileset.NOTHING) {
+                        world[x - 1][y - 1] = Tileset.WALL;
+                    }
+                    if (x < WIDTH - 1 && y > 0 && world[x + 1][y - 1] == Tileset.NOTHING) {
+                        world[x + 1][y - 1] = Tileset.WALL;
+                    }
+                    if (x > 0 && y < HEIGHT - 1 && world[x - 1][y + 1] == Tileset.NOTHING) {
+                        world[x - 1][y + 1] = Tileset.WALL;
+                    }
+                    if (x < WIDTH - 1 && y < HEIGHT - 1 && world[x + 1][y + 1] == Tileset.NOTHING) {
+                        world[x + 1][y + 1] = Tileset.WALL;
+                    }
+                }
+            }
+        }
+    }
+}
